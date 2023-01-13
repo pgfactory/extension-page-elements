11759df036d6a653875608c94f483b60dbc46fe5
Dieter Stokar <inbox@usility.ch>
2023-01-13 20:32:43 +0100

    _finalCode: - bugfix re TransVars -> TwigVars

--- src/_finalCode.php
+++ src/_finalCode.php
@@ -9,7 +9,7 @@
 
 namespace Usility\PageFactoryElements;
 use Usility\PageFactory\PageFactory as PageFactory;
-use Usility\PageFactory\TransVars as TransVars;
+use Usility\PageFactory\TwigVars as TwigVars;
 
 use function \Usility\PageFactory\writeFile;
 use function \Usility\PageFactory\translateToIdentifier;
@@ -17,7 +17,7 @@ use function \Usility\PageFactory\getDir;
 use function \Usility\PageFactory\fileTime;
 use function \Usility\PageFactory\getFile;
 
-$transVars =    TransVars::$transVars;
+$transVars =    TwigVars::$transVars;
 $translated = [];
 $files = getDir(PAGE_ELEMENTS_PATH.'js/*.js');
 foreach ($files as $file) {
