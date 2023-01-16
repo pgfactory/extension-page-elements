8cfa09a116f301eabf4c571fa0cbbe6e30f7a276
Dieter Stokar <inbox@usility.ch>
2023-01-16 16:33:08 +0100

    PageElements: - adapted to new macro scheme

--- src/PageElements.php
+++ src/PageElements.php
@@ -2,33 +2,81 @@
 
 namespace Usility\PageFactoryElements;
 use Usility\PageFactory\PageFactory as PageFactory;
+use Usility\PageFactory\Scss as Scss;
+use function \Usility\PageFactory\getDir;
 
 
 
 class PageElements
 {
+    /**
+     * @param $pfy
+     */
     public function __construct($pfy)
     {
         $this->pfy = $pfy;
         $this->pg = PageFactory::$pg;
         $this->assets = PageFactory::$assets;
-        $this->trans = PageFactory::$trans;
+
         $this->extensionPath = dirname(dirname(__FILE__)).'/';
+        $this->initMacros();
+        $this->updateScss();
     } // __construct
 
-    public function getAssetDefs()
+
+    /**
+     * @return void
+     */
+    private function initMacros(): void
+    {
+        $dir = getDir($this->extensionPath.'macros/*.php');
+        foreach ($dir as $file) {
+            if (basename($file[0] !== '#')) {
+                require_once $file;
+            }
+        }
+    } // initMacros
+
+
+    /**
+     * @return void
+     */
+    private function updateScss(): void
+    {
+        $dir = getDir($this->extensionPath.'scss/*.scss');
+        $targetPath = $this->extensionPath.'assets/css/';
+        foreach ($dir as $file) {
+            if (basename($file[0] !== '#')) {
+                Scss::updateFile($file, $targetPath);
+            }
+        }
+    } // updateScss
+
+
+    /**
+     * @return array
+     */
+    public function getAssetDefs(): array
     {
         return PE_URL_DEFINITIONS;
     } // getAssetDefs
 
 
-    public function getAssetGroups()
+    /**
+     * @return array
+     */
+    public function getAssetGroups(): array
     {
         return PE_ASSET_GROUPS;
     } // getAssetGroups
 
 
-    protected function addAssets($assets) {
+    /**
+     * @param $assets
+     * @return void
+     */
+    protected function addAssets($assets): void
+    {
         $this->assets->addAssets($assets);
     } // addAssets
 } // PageElements
