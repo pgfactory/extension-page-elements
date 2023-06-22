07827cd351b1e57809b333f495dc86d0da1deef1
Dieter Stokar <zurisee@gmail.com>
2023-06-22 14:38:24 +0200

    PE index.php: - routes for access-codes removed (obsolete, replaced by ?a=)

--- index.php
+++ index.php
@@ -18,28 +18,6 @@ if (basename(dirname(__FILE__))[0] === '#') {
 }
 
 Kirby::plugin('pgfactory/pagefactory-pageelements', [
-    'routes' => [
-        [
-            // catch tokens in URLs: (i.e. all capital letter or digit codes like 'p1/A1B2C3')
-            'pattern' => '(:all)',
-            'action'  => function ($slug) {
-                // check pattern 'p1/ABCDEF':
-                if (preg_match('|^(.*?) / ([A-Z]{5,15})$|x', $slug, $m)) {
-                    $slug = $m[1];
-                    PageFactory::$slug = $slug;
-                    PageFactory::$urlToken = $m[2];
-
-                // check pattern 'ABCDEF', i.e. page without slug:
-                } elseif (preg_match('|^ ([A-Z]{5,15})$|x', $slug, $m)) {
-                    PageFactory::$slug = '';
-                    PageFactory::$urlToken = $m[1];
-                    return site()->visit(page()); //ToDo: fix
-                }
-                return $this->next();
-            }
-        ],
-    ],
-
     'hooks' => [
         'route:before' => function (\Kirby\Http\Route $route, string $path) {
             // intercept serverLog request: ?log
