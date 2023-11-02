13c952823e891c8e7b711a37e35b9ff496d5e2c6
Dieter Stokar <zurisee@gmail.com>
2023-11-02 20:00:34 +0100

    _doc: - cleanup

--- _doc/site_config_config.php
+++ _doc/site_config_config.php
@@ -1,5 +1,8 @@
 
 
     'pgfactory.pagefactory-elements.options' => [
-		'log-ip'		=> true, // whether serverLog() shall include IP of agent in log,
+  		'log-ip'		=> true, // whether serverLog() shall include IP of agent in log,
+//        'activatePresentationSupport' => true,
+//        'presentationAutoSizing'      => true,
+//        'presentationDefaultSize'     => '1.8vw',
     ],
