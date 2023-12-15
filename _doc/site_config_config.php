b06b64bd72303b49c4fb61bd48c0ceaab49ee1bd
Dieter Stokar <inbox@usility.ch>
2023-12-15 11:02:17 +0100

    Login: - self change password overhauled

--- _doc/site_config_config.php
+++ _doc/site_config_config.php
@@ -1,8 +1,40 @@
 
 
     'pgfactory.pagefactory-elements.options' => [
-  		'log-ip'		=> true, // whether serverLog() shall include IP of agent in log,
+        'allowChangePassword' => true,
+//  		'log-ip'		=> true, // whether serverLog() shall include IP of agent in log,
 //        'activatePresentationSupport' => true,
 //        'presentationAutoSizing'      => true,
 //        'presentationDefaultSize'     => '1.8vw',
     ],
+
+## User Self Admin:
+
+Prerequisite:
+site/blueprints/users/role.yml must contain:
+
+---
+permissions:
+    access:
+        account: true
+        site: false
+        languages: false
+        system: false
+        users: false
+    user:
+        changeEmail: true
+        changeLanguage: false
+        changeName: true
+        changePassword: true
+        changeRole: false
+        delete: true
+        update: true
+---
+
+
+
+
+Using config options:
+
+    kirby()->option('pgfactory.pagefactory-elements.options.allowChangePassword');
+
