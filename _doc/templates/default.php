a29da2e75146dd0406dd312addbe7cf1719d320e
Dieter Stokar <zurisee@gmail.com>
2025-05-28 10:13:28 +0200

    PE install docs: updated

---
+++ _doc/templates/default.php
@@ -0,0 +1,89 @@
+<!doctype html><?= $page->cacheIndicator() ?>
+<html lang="<?= $page->lang() ?>">
+<head>
+  <meta charset="utf-8">
+  <title><?= $page->headTitle() ?></title>
+
+  <base href="<?= $page->baseUrl() ?>">
+  <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1">
+  <meta name="generator" content="<?= $page->generator() ?>)">
+  <?php snippet('prevnextlinks', ['args' => "type:'header-links'"]) ?>
+  <?php snippet('favicon') ?>
+
+  <?= $page->headInjections() ?>
+</head>
+
+<body id='pfy' class='pfy-default-styling pfy-auto-tabulator <?= $page->bodyTagClasses() ?>' <?= $page->bodyTagAttributes() ?>>
+
+<div class='pfy-page'>
+
+  <div class="pfy-header-wrapper">
+
+    <header class='pfy-header'>
+      <div class="pfy-large-screen-only v-align-with-nav">Title defined in Template</div>
+    </header>
+
+    <div class="pfy-nav-outer-wrapper">
+         <?php snippet('nav', ['args' => "type:top, wrapperClass: 'pfy-nav-top-right-aligned pfy-nav-colored pfy-mobile-nav-colored'" ]); ?>
+    </div>
+
+  </div><!-- /pfy-header-wrapper -->
+
+  <?php snippet('prevnextlinks', ['args' => "'wrapperClass':'pfy-large-screen-only pfy-full-width'"]) ?>
+
+
+
+  <div class="pfy-main-wrapper">
+
+    <!-- === page content =============== -->
+    <main id='main' class='pfy-main'>
+
+
+      <?= $page->pageContent() ?>
+
+    </main>
+    <!-- === /page content =============== -->
+
+
+
+    <footer class="pfy-footer">
+
+      <div class="pfy-footer-sitemap pfy-full-width">
+
+        <?php snippet('nav', ['args' => 'type:sitemap']); ?>
+
+        <?php snippet('prevnextlinks', ['args' => "'wrapperClass':'pfy-full-width', center:'%loginButton%'"]) ?>
+
+      </div>
+
+     <?php snippet('countvisits', ['args' => "show:loggedin|localhost, prefix:Anz. Besuche:, suffix:(seit %since%), dontCount:loggedin"]) ?>
+
+<?php if ($page->localhost()->value): ?>
+
+      <div class="dev-footer">
+        <div>
+          <?= $page->adminPanelLink() ?>
+        </div>
+        <div>
+        </div>
+        <div>
+          [<?php snippet('link', ['args' => "url: '~page/?dev=reset', text: 'dev auto'"]) ?> |
+           <?php snippet('link', ['args' => "url: '~page/?dev=false', text: 'dev off'"]) ?> |
+          <?php snippet('link', ['args' => "url: '~page/?reset', text: 'reset'"]) ?>]
+        </div>
+      </div>
+<?php endif ?>
+    </footer>
+
+
+
+  <?= $page->smallScreenHeader() ?>
+
+  </div><!-- /pfy-main-wrapper -->
+
+</div><!-- /.pfy-page -->
+
+<?= $page->bodyEndInjections() ?>
+</body>
+</html>
+
