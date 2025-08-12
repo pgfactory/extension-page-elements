4fe095615432f7a37a7f281cb0658c6d59fc40f1
Dieter Stokar <zurisee@gmail.com>
2025-08-12 16:32:56 +0200

    SwissMap/MacSearch: - pot. breaking: args modified - customPOI support improved

--- macros/swissmap.php
+++ macros/swissmap.php
@@ -4,7 +4,12 @@ namespace PgFactory\PageFactory;
 require_once __DIR__ . "/../src/MapSearch.php";
 
 /*
- * PageFactory Macro (and Twig Function)
+ * PageFactory Macro
+ *
+ * Uses search.ch/map
+ *  -> Doc: https://search.ch/map/api/help
+ *
+ * Icons: https://search.ch/map/images/icons/21/
  */
 
 return function ($options = '')
@@ -13,17 +18,19 @@ return function ($options = '')
     // Definition of arguments and help-text:
     $config =  [
         'options' => [
-            'lat' 	=> ["[float] Specifies latitude of center of map", ''],
-            'location' 	=> ["Specifies what the map is to center on: either an address (e.g. 'bahnhofstr. 1, zürich' or ".
-                "coordinates (e.g. '47.36751, 8.53988'), see https://map.search.ch/api/help#geocoding", null],
-            'metersPerPixel' 	=> ["[512 .. 0.125] Defines the zoom level as 'meters per pixel'", null],
-            'zoom' 	=> ["Synonyme for 'metersPerPixel'", null],
-            'id' 	=> ["[string] Defines the ID to be applied to the map container", null],
-            'minHeight' 	=> ["[string] ", '200px'],
-            'mapType' 	=> ["[street|satellite] Specifies in what way the map shall be displayed initially", 'street'],
-            'controls' 	=> ["[zoom,type,ruler,all] Specifies which controls are active", 'all'],
-            'poigroups' 	=> ["Specifies which points-of-interests are displayed, see https://map.search.ch/api/classref#poigroups", null],
-            'customPOIs' 	=> ["[comma seperated list on locations|'file:'] Defines points-of-interest to be displayed. ".
+            'location' 	        => ["Specifies what the map is to center on: either an address (e.g. 'bahnhofstr. 1, zürich' or ".
+                "coordinates (e.g. '47.36751, 8.53988'), <br>see https://map.search.ch/api/help#geocoding", null],
+            'lat' 	            => ["[float] Specifies latitude of center of map.", null],
+            'long' 	            => ["[float] Specifies longitude of center of map.", null],
+            'zoom' 	            => ["[6 - 19] Defines the zoom level.", 14],
+            'id' 	            => ["[string] Defines the ID to be applied to the map container", null],
+            'minHeight' 	    => ["[string] Defines the minimum height of the presented map.", '200px'],
+            'height' 	        => ["[string] Defines the minimum height of the presented map.", null],
+            'mapType' 	        => ["[street|satellite] Specifies in what way the map shall be displayed initially.", 'street'],
+            'controls' 	        => ["[zoom,type,ruler,all] Specifies which controls are active", 'all'],
+            'poigroups' 	    => ["Specifies which points-of-interests are displayed. E.g. 'poigroups: \'haltestelle,restaurant,webcam\'.' ".
+                "<br>see https://map.search.ch/api/classref#poigroups", null],
+            'customPOIs' 	    => ["[comma seperated list of locations|'file:'] Defines points-of-interest to be displayed. ".
                 "A single POI may be supplied as a comma-separated-list; multiple POIs via a .csv file. ".
                 "Structure: 'location,title,description,icon'", null],
             'customPOIIcon' 	=> ["Defines the default icon to represent custom locations.", null],
@@ -54,6 +61,10 @@ EOT,
 
     // assemble output:
     $mapSearch = new MapSearch();
+    if ($options['location'] === 'icons') {
+        return $mapSearch->listIcons();
+    }
+
     $html .= $mapSearch->render($options);
 
     return $html;
--- src/MapSearch.php
+++ src/MapSearch.php
@@ -2,11 +2,21 @@
 
 namespace PgFactory\PageFactory;
 
+const MAP_SEARCH_ICON_PATH = 'https://search.ch/map/images/icons/21/';
+const LOCAL_ICON_PATH = '~/assets/icons/';
 
 class MapSearch
 {
     private $inx;
     private $customPOIIcon;
+    private string $localIconsPath;
+    private string $localIconsUrl;
+
+    public function __construct()
+    {
+        $this->localIconsPath = resolvePath(LOCAL_ICON_PATH);
+        $this->localIconsUrl = Utils::resolveUrls(LOCAL_ICON_PATH);
+    }
 
 
     /**
@@ -18,7 +28,61 @@ class MapSearch
     {
         $this->inx = $options['inx'];
 
-        $id = ($options['id']??false) ?: "pfy-swissmap-container-{$this->inx}";
+        $options = $this->parseOptions($options);
+
+        $out = "<div id='{$options['id']}' class='pfy-swissmap-container' data-map-inx='$this->inx'></div>\n";
+
+        if ($this->inx === 1) {
+            Page::addJs('const pfyMaps = [];');
+            Assets::addAssets('https://search.ch/map/api/map.js');
+        }
+
+        $centerpois = ($options['centerpois']??true) ? "\n    centerpois:true,": '';
+
+        $map = "pfyMaps[{$this->inx}]";
+
+        $jq = <<<EOT
+
+$map = new SearchChMap({
+    container: '{$options['id']}',
+    center: {$options['location']},
+    zoomlevel: {$options['zoom']},
+    {$options['attributes']}$centerpois
+});{$options['customPOIs']}
+
+
+EOT;
+
+        Page::addJsReady($jq);
+
+        $this->renderCss($options);
+
+        return $out;
+    } // render
+
+
+    private function renderCss($options): void
+    {
+        $cssRules = '';
+        $minHight = ($options['minHeight']??false) ?: '200px';
+        if ($minHight) {
+            $cssRules = "min-height: $minHight;";
+        }
+        $hight = $options['height']??false;
+        if ($hight) {
+            $cssRules .= "height: $hight;";
+        }
+        if ($cssRules) {
+            Page::addCss("#{$options['id']} { $cssRules }");
+        }
+    } // renderCss
+
+
+    private function parseOptions(array $options): array
+    {
+        $res = [];
+
+        $res['id'] = ($options['id']??false) ?: "pfy-swissmap-container-{$this->inx}";
         $location = ($options['location']??false) ?: '';
         $long = ($options['long']??false) ?: '';
         $lat = ($options['lat']??false) ?: '';
@@ -30,11 +94,9 @@ class MapSearch
         } else {
             $location = $this->getLocation($location);
         }
+        $res['location'] = $location;
 
-        if ($options['metersPerPixel']??false) {
-            $options['zoom'] = $options['metersPerPixel'];
-        }
-        $zoom = ($options['zoom']??false) ?: 0;
+        $res['zoom'] = ($options['zoom']??false) ?: 6;
 
         if (($options['mapType']??false) && (strpos(",aerial,street,satellite,", ",{$options['mapType']},") !== false)) {
             if ($options['mapType'] === 'satellite') {
@@ -64,9 +126,15 @@ EOT;
             $controls = "\tcontrols: '$controls',\n";
         }
 
-        $poigroups = ($options['poigroups']??false) ?: '';
-        if ($poigroups) {
+        // POIgroups:
+        $poigroups = isset($options['poigroups']) ? $options['poigroups'] : null;
+        if ($poigroups === false) {
+            $poigroups = "\tpoigroups: '-',\n"; // show no POIgroups at all
+        } elseif ($poigroups !== null) {
             $poigroups = "\tpoigroups: '$poigroups',\n";
+        }
+        if ($poigroups) {
+            $res['poigroups'] = $poigroups;
         }
 
         $this->customPOIIcon = ($options['customPOIIcon']??false) ?: '';
@@ -74,6 +142,7 @@ EOT;
         if ($customPOIs) {
             $customPOIs = $this->handleCustomPOIs($customPOIs);
         }
+        $res['customPOIs'] = $customPOIs;
 
         $drawing = ($options['drawing']??false) ?: '';
         if ($drawing) {
@@ -87,38 +156,14 @@ EOT;
         if ($gestureHandling) {
             $gestureHandling = "\tgestureHandling: '$gestureHandling',\n";
         }
+        $res['gestureHandling'] = $gestureHandling;
+        $res['attributes'] = "$marker$mapType$route$controls$poigroups$drawing$gestureHandling";
 
-        $out = "<div id='$id' class='pfy-swissmap-container'></div>\n";
+        $res['height'] = $options['height']??false;
+        $res['minHeight'] = $options['minHeight']??false;
 
-        if ($this->inx === 1) {
-            Assets::addAssets('https://search.ch/map/api/map.js');
-        }
-
-        $minHight = ($options['minHeight']??false) ?: '200px';
-        $cssRules = "min-height: $minHight;";
-        if (($options['height']??false)) {
-            $cssRules .= "height: {$options['height']};";
-        }
-        Page::addCss("#$id { $cssRules }");
-
-        $map = "map{$this->inx}";
-
-        $jq = <<<EOT
-
-$map = new SearchChMap({
-    container: '$id',
-    center: $location,
-    zoom: $zoom,
-    $marker$mapType$route$controls$poigroups$drawing$gestureHandling
-});$customPOIs
-
-
-EOT;
-
-        Page::addJsReady($jq);
-
-        return $out;
-    } // render
+        return $res;
+    } // parseOptions
 
 
     /**
@@ -131,7 +176,7 @@ EOT;
         // '1.00, 2.00'
         // '[1.00, 2.00]'
         //
-        if (preg_match('/^\s* \[? \s* (\d+\.?\d*) \s*,\s* (\d+\.?\d*) \s* \]? \s*$/x', $str, $m)) {
+        if (preg_match('/^\s* \[? \s* (\d+\.?\d*) \s*,\s* (\d+\.?\d*) \s* ]? \s*$/x', $str, $m)) {
             $str = "[{$m[1]}, {$m[2]}]";
         } else {
             $str = "'$str'";
@@ -146,37 +191,279 @@ EOT;
      * @return string
      * @throws \Exception
      */
-    private function handleCustomPOIs(string $customPOIs): string
+    private function handleCustomPOIs(mixed $customPOIs): string
     {
         $jq = '';
         $map = "map{$this->inx}";
 
-        if (preg_match('/^(\w+):(.*)/', $customPOIs, $m)) {
-            if ($m[1] === 'file') {
-                $file = resolvePath($m[2], true);
-                if (file_exists($file)) {
-                    $db = new DataSet($file);
-                    $recs = $db->read();
-                    foreach ($recs as $rec) {
-                        $location = trim($this->getLocation($rec[0]),'"\'');
-                        $title = trim($rec[1],'"\'');
-                        $description = trim($rec[2],'"\'');
-                        $poiIcon = isset($rec[3]) ? trim($rec[3],'"\''): $this->customPOIIcon;
-                        $jq .= <<<EOT
-
-    $map.addPOI(new SearchChPOI({ 
-        center:'$location', 
-        title:'$title', 
-        html:'$description', 
-        icon:'$poiIcon' 
-    }));
-
+        if (is_array($customPOIs)) {
+            $location = $customPOIs['center'] ?? '';
+            $title = $customPOIs['title'] ?? '';
+            $description = $customPOIs['description'] ?? ($customPOIs['html'] ?? '');
+            $poiIcon = isset($customPOIs['icon']) ?: $this->customPOIIcon;
+            $jq .= <<<EOT
+    
+        $map.addPOI(new SearchChPOI({ 
+            center: $location, 
+            title:'$title', 
+            html:'$description', 
+            icon:'$poiIcon' 
+        }));
+    
 EOT;
+
+        } elseif (is_string($customPOIs)) {
+            if (preg_match('/^(\w+):(.*)/', $customPOIs, $m)) {
+                if ($m[1] === 'file') {
+                    $file = resolvePath($m[2]);
+                    if (file_exists($file)) {
+                        $db = new DataSet($file);
+                        $recs = $db->read();
+                        foreach ($recs as $rec) {
+                            if (isset($rec[0])) {
+                                $location = trim($this->getLocation($rec[0]), '"\'');
+                                if (preg_match('/^\[([\d.]+)\s*,\s*([\d.]+)]$/', $location, $m)) {
+                                    $location = "[$m[1], $m[2]]";
+                                } else {
+                                    $location = "'$location'";
+                                }
+                                $title = trim($rec[1], '"\'');
+                                $description = trim($rec[2], '"\'');
+                                $poiIcon = isset($rec[3]) ? trim($rec[3], '"\'') : $this->customPOIIcon;
+                            } else {
+                                $location = $rec['center'] ?? '';;
+                                if (preg_match('/([\d.]+)\s*,\s*([\d.]+)/', $location, $m)) {
+                                    $location = "[$m[1], $m[2]]";
+                                } else {
+                                    $location = "'$location'";
+                                }
+                                $title = trim($rec['title'] ?? '', '"\'');
+                                $description = trim($rec['html'] ?? '', '"\'');
+                                $poiIcon = ($rec['icon'] ?? false) ?: $this->customPOIIcon;
+                            }
+                            if ($poiIcon) {
+                                if (!fileExt($poiIcon)) {
+                                    $poiIcon .= '.png';
+                                }
+                                if (!str_contains($poiIcon, '/')) {
+                                    if (file_exists($this->localIconsPath . $poiIcon)) {
+                                        $poiIcon = $this->localIconsUrl . $poiIcon;
+                                    } else {
+                                        $poiIcon = MAP_SEARCH_ICON_PATH . $poiIcon;
+                                    }
+                                }
+                                $poiIcon = "icon:'$poiIcon'";
+                            }
+                            $jq .= <<<EOT
+    
+        $map.addPOI(new SearchChPOI({ 
+            center: $location, 
+            title:'$title', 
+            html:'$description', 
+            $poiIcon
+        }));
+    
+EOT;
+                        }
                     }
                 }
             }
         }
         return $jq;
     } // handleCustomPOIs
+
+
+    public function listIcons(): string
+    {
+        $poiIcons = [
+            'anzeigen.png',
+            'apotheke.png',
+            'autoverlad.png',
+            'autoverlad_g.png',
+            'autoverlad_r.png',
+            'bad.png',
+            'bar.png',
+            'baustelle.png',
+            'berg.png',
+            'bergbahn.png',
+            'bus.png',
+            'business.png',
+            'business_p.png',
+            'cafe.png',
+            'camping.png',
+            'feuerstelle.png',
+            'freibad.png',
+            'fusspass.png',
+            'gastro.png',
+            'gebaeude.png',
+            'geldautomat.png',
+            'geo.png',
+            'gesperrt.png',
+            'gondelbahn.png',
+            'grill.png',
+            'haltestelle.png',
+            'hotel.png',
+            'immo.png',
+            'kino.png',
+            'kirche.png',
+            'kultur.png',
+            'ladestation.png',
+            'ladestation_g.png',
+            'ladestation_o.png',
+            'ladestation_r.png',
+            'luftseilbahn.png',
+            'maplink.png',
+            'meteoradar.png',
+            'mobility.png',
+            'museum.png',
+            'p+rail.png',
+            'p0.png',
+            'p1.png',
+            'p100.png',
+            'p2.png',
+            'p3.png',
+            'p4.png',
+            'p5.png',
+            'p6.png',
+            'p7.png',
+            'p8.png',
+            'p9.png',
+            'park_and_rail.png',
+            'parkandrail.png',
+            'parkhaus.png',
+            'parkhaus_g.png',
+            'parkhaus_o.png',
+            'parkhaus_r.png',
+            'parking.png',
+            'parkplatz.png',
+            'pass.png',
+            'person.png',
+            'polizei.png',
+            'post.png',
+            'post_g.png',
+            'post_r.png',
+            'restaurant.png',
+            'route_bus.png',
+            'route_car.png',
+            'route_change.png',
+            'route_end.png',
+            'route_ship.png',
+            'route_start.png',
+            'route_tram.png',
+            'route_via.png',
+            'route_walk.png',
+            'route_zug.png',
+            'schienenbahn.png',
+            'schiff.png',
+            'schule.png',
+            'scooter.png',
+            'scooter_g.png',
+            'scooter_o.png',
+            'scooter_r.png',
+            'service.png',
+            'sesselbahn.png',
+            'shop.png',
+            'skilift.png',
+            'spielplatz.png',
+            'spital.png',
+            'stau.png',
+            'tankstelle.png',
+            'tel.png',
+            'tel_business.png',
+            'tel_private.png',
+            'temp_1.png',
+            'temp_10.png',
+            'temp_11.png',
+            'temp_12.png',
+            'temp_13.png',
+            'temp_14.png',
+            'temp_15.png',
+            'temp_16.png',
+            'temp_17.png',
+            'temp_18.png',
+            'temp_19.png',
+            'temp_2.png',
+            'temp_20.png',
+            'temp_21.png',
+            'temp_22.png',
+            'temp_23.png',
+            'temp_24.png',
+            'temp_25.png',
+            'temp_26.png',
+            'temp_27.png',
+            'temp_28.png',
+            'temp_29.png',
+            'temp_3.png',
+            'temp_30.png',
+            'temp_31.png',
+            'temp_32.png',
+            'temp_33.png',
+            'temp_34.png',
+            'temp_35.png',
+            'temp_36.png',
+            'temp_37.png',
+            'temp_38.png',
+            'temp_39.png',
+            'temp_4.png',
+            'temp_40.png',
+            'temp_41.png',
+            'temp_42.png',
+            'temp_5.png',
+            'temp_6.png',
+            'temp_7.png',
+            'temp_8.png',
+            'temp_9.png',
+            'theater.png',
+            'tour.png',
+            'tour_alpin.png',
+            'tour_inlineskating.png',
+            'tour_jogging.png',
+            'tour_mountainbike.png',
+            'tour_nordicwalking.png',
+            'tour_ort.png',
+            'tour_ski.png',
+            'tour_velo.png',
+            'tour_wanderung.png',
+            'tour_winterwanderung.png',
+            'tram.png',
+            'tunnel_g.png',
+            'tunnel_r.png',
+            'umleitung.png',
+            'velo.png',
+            'velo_g.png',
+            'velo_o.png',
+            'velo_r.png',
+            'veloland.png',
+            'verkehr.png',
+            'verwaltung.png',
+            'viasuisse.png',
+            'vinfo.png',
+            'vpass.png',
+            'vpass_g.png',
+            'vpass_r.png',
+            'wasserfall.png',
+            'webcam.png',
+            'wikipedia.png',
+            'winter.png',
+            'zug.png',
+        ];
+        $out = '';
+        foreach ($poiIcons as $icon) {
+            $iconName = basename($icon, '.png');
+            $out .= "<li>$iconName: <img src='".MAP_SEARCH_ICON_PATH."$icon' width='21' height='21' alt='$icon' /></li>\n";
+        }
+        $out = "<h2>MapSearch Icons</h2>\n</h2><ul class='pfy-mapsearch-icons'>\n$out</ul>\n";
+        Page::$css .= <<<EOT
+.pfy-mapsearch-icons {
+    list-style: none;
+}
+.pfy-mapsearch-icons li {
+    display: inline-block;
+    width: 12em;
+}
+EOT;
+
+        return $out;
+    } // listIcons
 
 } // MapSearch
