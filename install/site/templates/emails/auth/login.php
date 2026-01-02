b2ce8bcfdd5f444be01680330087660bdb36a3f6
Dieter Stokar <zurisee@gmail.com>
2026-01-02 14:33:34 +0100

    Passwordless login: - fix re mail templates for login and password-reset

--- install/site/templates/emails/auth/login.php
+++ install/site/templates/emails/auth/login.php
@@ -10,8 +10,43 @@ use PgFactory\PageFactory\Page;
 use PgFactory\PageFactory\TransVars;
 use function \PgFactory\PageFactory\mylog;
 
+const MAIL_BODY = [
+  'de' => <<<EOT
+Hallo %user%,
+
+Sie haben kürzlich einen Login Code angefordert für die Website %url%.
+Der folgende Login Code wird für die nächsten %timeout% Minuten gültig sein:
+
+%code%
+
+Falls Sie keinen Login Code angefordert haben, können Sie diese E-Mail ignorieren.
+Kontaktieren Sie den %webmaster%, wenn Sie Fragen haben.
+Aus Sicherheitsgründen sollten Sie diese E-Mail bitte NICHT weiterleiten.
+EOT,
+  '_' => <<<EOT
+Hi %user%,
+
+You recently requested a login code for the website %url%.
+The following login code will be valid for %timeout% minutes:
+
+%code%
+
+If you did not request a login code, please ignore this email or contact your administrator if you have questions.
+For security, please DO NOT forward this email.
+
+EOT,
+];
+
 $text = TransVars::getVariable('pfy-login-code-mail-body');
+if (!$text) {
+  // when loggin in to the panel, TransVar is not available at this point, so we short cut:
+  $lang = kirby()->language() ?? kirby()->defaultLanguage();
+  $text = (MAIL_BODY[$lang]??false) ?: MAIL_BODY['_'];
+}
 $webmasterEmail = PageFactory::$webmasterEmail;
+if (!$webmasterEmail) {
+  $webmasterEmail = kirby()->option('pgfactory.pagefactory.webmaster_email') ?: '';
+}
 $code = str_replace(' ', '', $code);
 
 $text = str_replace(
@@ -36,7 +71,7 @@ if (Permission::isLocalhost()) {
     Page::setPopup($popup, 'Login Code E-Mail');
 }
 
-if (PageFactory::$dev) {
+if (PageFactory::$dev??false) {
     mylog("Login email prepared with code '$code'.", 'login-log.txt');
 }
 echo $text;
--- install/site/templates/emails/auth/password-reset.php
+++ install/site/templates/emails/auth/password-reset.php
@@ -10,8 +10,42 @@ use PgFactory\PageFactory\Page;
 use PgFactory\PageFactory\TransVars;
 use function \PgFactory\PageFactory\mylog;
 
+const MAIL_BODY = [
+  'de' => <<<EOT
+    Hallo %user%,
+    
+    Sie haben kürzlich einen Passwort Reset angefordert für die Website %url%.
+    Der folgende Passwort Reset Code wird für die nächsten %timeout% Minuten gültig sein:
+    
+    %code%
+
+    Falls Sie keinen Passwort Reset angefordert haben, können Sie diese E-Mail ignorieren.
+    Kontaktieren Sie den %webmaster%, wenn Sie Fragen haben.
+
+EOT,
+  '_' => <<<EOT
+Hi %user%,
+
+You recently requested a password reset code for the website %url%.
+The following password reset code will be valid for %timeout% minutes:
+
+%code%
+
+If you did not request a password reset code, please ignore this email or contact your administrator if you have questions.
+
+EOT,
+];
+
 $text = TransVars::getVariable('pfy-login-pw-reset-mail-body');
+if (!$text) {
+  // when loggin in to the panel, TransVar is not available at this point, so we short cut:
+  $lang = kirby()->language() ?? kirby()->defaultLanguage();
+  $text = (MAIL_BODY[$lang]??false) ?: MAIL_BODY['_'];
+}
 $webmasterEmail = PageFactory::$webmasterEmail;
+if (!$webmasterEmail) {
+  $webmasterEmail = kirby()->option('pgfactory.pagefactory.webmaster_email') ?: '';
+}
 $code = str_replace(' ', '', $code);
 
 $text = str_replace(
@@ -36,7 +70,7 @@ if (Permission::isLocalhost()) {
   Page::setPopup($popup, 'Password Reset E-Mail');
 }
 
-if (PageFactory::$dev) {
-    mylog("Login email prepared with code '$code'.", 'login-log.txt');
-}
+//if (PageFactory::$dev) {
+//    mylog("Login email prepared with code '$code'.", 'login-log.txt');
+//}
 echo $text;
