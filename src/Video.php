7ab09d77aeca4c60149f027e4bf2a511d53503ed
Dieter Stokar <inbox@usility.ch>
2026-04-04 08:57:23 +0200

    Video.php: overhauled

--- src/Video.php
+++ src/Video.php
@@ -5,98 +5,46 @@ namespace PgFactory\PageFactoryElements;
 use PgFactory\PageFactory\Assets;
 use PgFactory\PageFactory\Utils;
 use function PgFactory\PageFactory\explodeTrim;
+use function PgFactory\PageFactory\getDir;
 
 class Video
 {
-    private static int $inx = 0;
+    private static $inx = 0;
 
-    public static function render(array $options): string
+    public static function render($options)
     {
         self::$inx++;
+
         self::loadAssets();
 
-        if ($options['youtube'] ?? false) {
+        // variant youtube video:
+        if ($options['youtube']??false) {
             return self::renderYoutube($options);
+
+        } else {
+            // variant locally stored video:
+            return self::renderLocalVideo($options);
         }
-        return self::renderLocalVideo($options);
     } //  render
 
 
-    private static function renderLocalVideo(array $options): string
+
+    private static function renderLocalVideo($options): string
     {
+        $html = '';
+        $attributes = '';
+        $style = '';
         $inx = self::$inx;
-        $title = ($options['title'] ?? false) ? " title='{$options['title']}'" : '';
-        $class = ($options['class'] ?? '') ?: ($options['wrapperClass'] ?? '');
+        $title = $options['title'] ? " title='{$options['title']}'" : '';
+        $class = $options['class'] ?: $options['wrapperClass'];
         $class = $class ? " $class" : '';
-        $startAt = ($options['startAt'] ?? false) ? "#t={$options['startAt']}" : '';
-        $styles = self::getStyles($options);
+        $startAt = $options['startAt'] ? "#t={$options['startAt']}" : '';
 
         // determine source files:
-        $file = ($options['file'] ?? '') ?: ($options['src'] ?? '');
-        $files = self::resolveSourceFiles($file);
-
-        // render <source> tags:
+        $file = $options['file'] ?: $options['src'];
         $src = '';
-        foreach ($files as $f) {
-            $ext = pathinfo($f, PATHINFO_EXTENSION);
-            if (in_array($ext, PFY_SUPPORTED_VIDEO_FORMATS)) {
-                $src .= "    <source src='$f$startAt' type='video/$ext'>\n";
-            }
-        }
-
-        // misc attributes:
-        $attributes = '';
-        if ($poster = ($options['poster'] ?? false)) {
-            $attributes .= " poster=\"$poster\"";
-        }
-        if ($options['controls'] ?? false) {
-            $attributes .= ' controls';
-        }
-        if ($options['autoplay'] ?? false) {
-            $attributes .= ' autoplay muted';
-        } elseif ($options['muted'] ?? false) {
-            $attributes .= ' muted';
-        }
-        if ($options['loop'] ?? false) {
-            $attributes .= ' loop';
-        }
-
-        $videoHtml = <<<EOT
-  <video$attributes$title>
-$src    Your browser does not support the video tag.
-  </video>
-EOT;
-
-        return self::wrapContent($videoHtml, $inx, $class, $styles, $options['caption'] ?? false);
-    } // renderLocalVideo
-
-
-    private static function renderYoutube(array $options): string
-    {
-        $inx = self::$inx;
-        $title = ($options['title'] ?? false) ? " title='{$options['title']}'" : '';
-        $class = ($options['class'] ?? '') ?: ($options['wrapperClass'] ?? '');
-        $class = $class ? " $class" : '';
-        $styles = self::getStyles($options);
-
-        $youtube = $options['youtube'];
-        $startAt = ($options['startAt'] ?? false) ? "&amp;start={$options['startAt']}" : '';
-
-        $iframeHtml = <<<EOT
-  <iframe$styles src="https://www.youtube.com/embed/$youtube$startAt"$title allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-EOT;
-
-        return self::wrapContent($iframeHtml, $inx, $class, $styles, $options['caption'] ?? false);
-    } // renderYoutube
-
-
-    /**
-     * Resolves video source file(s) from a file path or comma-separated list.
-     */
-    private static function resolveSourceFiles(string $file): array
-    {
-        // case: no comma-separated list AND (no extension or last char is '*'):
-        if (!str_contains($file, ',') && (!(pathinfo($file, PATHINFO_EXTENSION)) || substr($file, -1) === '*')) {
+        // case 1: no comma-separated list, no extension or last car is '*':
+        if (!str_contains($file, ',') && !(pathinfo($file, PATHINFO_EXTENSION)) || (substr($file, -1) === '*')) {
             $path = dirname($file) . '/';
             $file = Utils::resolvePath(rtrim($file, '*'));
             $files = [];
@@ -111,38 +59,107 @@ EOT;
                     }
                 }
             }
-            return $files;
+            // case 2: comma-separated list:
+        } else {
+            $files = explodeTrim(',', $file);
+        }
+        // run through all source files and render <source> tags:
+        foreach ($files as $file) {
+            $ext = pathinfo($file, PATHINFO_EXTENSION);
+            if (in_array($ext, PFY_SUPPORTED_VIDEO_FORMATS)) {
+                $src .= "    <source src='$file$startAt' type='video/$ext'>\n";
+            }
         }
 
-        // case: comma-separated list or explicit file with extension:
-        return explodeTrim(',', $file);
-    } // resolveSourceFiles
+        $styles = self::getStyles($options);
 
+        // misc $attributes:
+        if ($poster = ($options['poster'] ?? false)) {
+            $poster = Utils::resolveUrl($poster);
+            $attributes .= " poster=\"$poster\"";
+        } else {
+            $files = getDir(basename($file));
+        }
+        if ($options['controls'] ?? false) {
+            $attributes .= ' controls';
+        }
+        if ($options['autoplay'] ?? false) {
+            $attributes .= ' autoplay muted';
+        } elseif ($options['muted'] ?? false) {
+            $attributes .= ' muted';
+        }
+        if ($options['loop'] ?? false) {
+            $attributes .= ' loop';
+        }
 
-    /**
-     * Wraps inner HTML in a figure (with caption) or div wrapper.
-     */
-    private static function wrapContent(string $innerHtml, int $inx, string $class, string $styles, string|false $caption): string
-    {
-        if ($caption) {
-            return <<<EOT
+        // assemble output:
+        if ($caption = ($options['caption'] ?? false)) {
+            $html .= <<<EOT
 
 <figure id="pfy-video-wrapper-$inx" class="pfy-video-wrapper$class"$styles>
-$innerHtml
+  <video$attributes$title>
+$src
+    Your browser does not support the video tag.
+  </video>
   <figcaption>$caption</figcaption>
 </figure><!-- /pfy-video-wrapper -->
 
 EOT;
-        }
 
-        return <<<EOT
+        } else {
+            $html .= <<<EOT
 
-<div id="pfy-video-wrapper-$inx" class="pfy-video-wrapper$class"$styles>
-$innerHtml
+<div id="pfy-video-wrapper-$inx" class="pfy-video-wrapper$class"$style>
+  <video$attributes$title>
+$src
+    Your browser does not support the video tag.
+  </video>
 </div><!-- /pfy-video-wrapper -->
 
 EOT;
-    } // wrapContent
+        }
+        return $html;
+    } // renderLocalVideo
+
+
+    private static function renderYoutube($options)
+    {
+        $youtube = $options['youtube'];
+        $inx = self::$inx;
+        $title = $options['title'] ? " title='{$options['title']}'" : '';
+        $class = $options['class'] ?: $options['wrapperClass'];
+        $class = $class ? " $class" : '';
+        $styles = self::getStyles($options);
+
+        $startAt = $options['startAt'] ? "&amp;start={$options['startAt']}" : '';
+        $html = <<<EOT
+<iframe $styles src="https://www.youtube.com/embed/$youtube$startAt"$title allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+EOT;
+
+        // assemble output:
+        if ($caption = ($options['caption'] ?? false)) {
+            $html = <<<EOT
+
+<figure id="pfy-video-wrapper-$inx" class="pfy-video-wrapper$class"$styles>
+$html
+  <figcaption>$caption</figcaption>
+</figure><!-- /pfy-video-wrapper -->
+
+EOT;
+
+        } else {
+            $html = <<<EOT
+
+<div id="pfy-video-wrapper-$inx" class="pfy-video-wrapper$class"$styles>
+$html
+</div><!-- /pfy-video-wrapper -->
+
+EOT;
+        }
+
+        return $html;
+    } // renderYoutube
 
 
     private static function loadAssets(): void
@@ -154,8 +171,8 @@ EOT;
         }
     } // loadAssets
 
-
-    private static function getStyles(array $options): string
+    
+    private static function getStyles($options): string
     {
         $style = '';
         if ($width = ($options['width'] ?? false)) {
@@ -167,7 +184,8 @@ EOT;
                 $style .= "width:auto;";
             }
         }
-        return $style ? ' style="' . $style . '"' : '';
+        $style = $style ? ' style="' . $style . '"' : '';
+        return $style;
     } // getStyles
 
 
