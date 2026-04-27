437ecb9b7fb1455b136a5289f8ceba55747a48ea
Dieter Stokar <inbox@usility.ch>
2026-04-27 15:38:43 +0200

    Macro download(): - support for single file added

--- macros/download.php
+++ macros/download.php
@@ -11,6 +11,8 @@ return function($argStr = '')
         'options' => [
             'path' => ['Selects the folder to be read. May include an optional '.
                 'selection pattern  (-> \'glob style\', e.g. "*.pdf" or "{&#92;&#42;.js,&#92;&#42;.css}")', null],
+            'file' => ['If `file` is defined, a download link for just this file is rendered', null],
+            'text' => ['If `file` is defined, this string is used as link text instead of the filename', null],
             'id' => ['Id to be applied to the enclosing li-tag (Default: pfy-dir-#)', null],
             'class' => ['Class to be applied to the enclosing li-tag (Default: pfy-dir)', 'pfy-dir'],
             'include' => ['[FILES,FOLDERS] Defines what to include in output', 'files'],
@@ -49,18 +51,53 @@ EOT,
         list($options, $str) = $str;
     }
 
-    if (!$options['template'] ?? false) {
-        $options['template'] = [
-            'element'=>"- (link: %download% text:%filename% type:%ext% target:_blank) %description%\n",
-            'folderElement'=> '<> <strong>%label%</strong>',
-            'markdown'=> true,
-        ];
+    if ($file = $options['file']) {
+        $filename = basename($file);
+        $text = $options['text'] ?: $filename;
+        $str .= "<a href='~page/?download=$filename' class='pfy-download-link'>$text</a>";
+        if ($_GET['download']??false) {
+            $requestedFile = $_GET['download'];
+            $requestedFilename = basename($requestedFile);
+            if (basename($file) === $requestedFilename) {
+                $path = dir_name($file);
+                $files = array_keys(getDirDeep($path, assoc:true));
+                if (in_array(basename($file), $files)) {
+                    Download::initiateDownload($file, $options['permission']);
+                }
+                return '';
+            }
+        }
+
+    } else {
+
+        if (!$options['template'] ?? false) {
+            $options['template'] = [
+                'element' => "- (link: %download% text:%filename% type:%ext% target:_blank) %description%\n",
+                'folderElement' => '<> <strong>%label%</strong>',
+                'markdown' => true,
+            ];
+        }
+
+        // assemble output:
+        $obj = new Dir();
+        $str .= $obj->render($options);
     }
 
-    // assemble output:
-    $obj = new Dir();
-    $str .= $obj->render($options);
-
+    // set up click handler that opens the download in a new tab:
+    if ($options['inx'] === 1) {
+        $js = <<<EOT
+pfyHandleEvent('a.pfy-download-link', ev => {
+  ev.preventDefault();
+  const downloadUrl = ev.target.href;
+  const newTab = window.open(downloadUrl, '_blank');
+  if (!newTab) {
+    // Popup blocker fallback:
+    window.location.href = downloadUrl;
+  }
+})
+EOT;
+        Page::addJsReady($js);
+    }
     return $str;
 };
 
