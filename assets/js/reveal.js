4735523b79e75b54d7f8ae342d8b529e90dae00e
Dieter Stokar <inbox@usility.ch>
2026-05-28 10:20:47 +0200

    reveal.js: overhauled

--- assets/js/reveal.js
+++ assets/js/reveal.js
@@ -11,8 +11,8 @@ let pfyRevealAccordionClickListenersInitialized = false;
 
 class RevealAccordion {
     static defaultOptions = {
-        controller: '.mdp-accordion-controller',
-        target: '.mdp-accordion-target',
+      controller: '[data-reveal-target]',
+        target: false,
         label: '',
         class: '',
         icon: false,
@@ -24,145 +24,231 @@ class RevealAccordion {
         clickListenersInitialized: false,
     };
 
-    static interactiveControllerTags = ['A', 'BUTTON'];
-
     constructor(customOptions = {}) {
         this.options = {...RevealAccordion.defaultOptions, ...customOptions};
         this.init();
     } // constructor
 
 
+  // === init =====================================================================
     init() {
-        const targetIds = this.prepareTargetElems();
-        this.initControllers(targetIds);
+        this.initControllers();
         this.setupGlobalListeners();
     } // init
 
-    prepareTargetElems() {
-        const targetIds = [];
-
-        domForAll(this.options.target, (targetEl) => {
-            pfyRevealAccordionInx++;
-            const uid = `pfy-reveal-${pfyRevealAccordionInx}`;
-            const details = this.wrapIntoDetailsElement(targetEl, uid);
-            this.initRequiredFormFields(details);
-            targetIds.push(uid);
-        });
-
-        return targetIds.join(' ');
-    } // prepareTargetElems
 
 
-    initControllers(targetIds) {
+    // === init controllers ======================================================
+    initControllers() {
       if (!this.options.controller) {
           return;
       }
+      const parent = this;
       domForAll(this.options.controller, (controllerEl) => {
-          controllerEl.setAttribute('aria-controls', targetIds);
-          controllerEl.classList.add('mdp-accordion-controller');
-          console.debug(`accordion controller initialized: #${controllerEl.id}`);
-          if (this.isClickableElement(controllerEl)) {
-              controllerEl.setAttribute('aria-pressed', 'false');
-              controllerEl.setAttribute('role', 'button');
-              this.setupGlobalClickListeners();
+        let targetSel = controllerEl.dataset.revealTarget;
+        if (!targetSel) {
+          if (parent.options.target) {
+            controllerEl.dataset.revealTarget = parent.options.target;
+            targetSel = parent.options.target;
+          } else {
+            console.error(`No accordion target defined`);
+            return;
           }
+        }
+        let targetIds = '';
+        domForAll(targetSel, (targetEl) => {
+          let id = targetEl.id;
+          if (!id) {
+            id = `pfy-reveal-target-${pfyRevealAccordionInx}`;
+            targetEl.id = id;
+          }
+          parent.prepareTargetElem(targetEl, id);
+          targetIds += `${id} `;
+        })
+
+        controllerEl.setAttribute('aria-controls', targetIds.trim());
+        controllerEl.classList.add('mdp-accordion-controller');
+        if (this.isStatelessTriggerElement(controllerEl)) {
+            controllerEl.setAttribute('aria-pressed', 'false');
+            controllerEl.setAttribute('role', 'button');
+        }
+//        console.debug(`accordion controller initialized: #${controllerEl.id}`);
       });
     } // initControllers
 
 
-    isClickableElement(el) {
-        return (RevealAccordion.interactiveControllerTags.includes(el.tagName) ||
-          (el.tagName === 'INPUT' && el.type === 'button'));
-    } // isClickableElement
 
 
+  // === prepare elements =======================================================
+  prepareTargetElem(targetEl, uid) {
+    if (targetEl.closest('.mdp-accordion-wrapper')) {
+      return; // already ininitialized
+    }
+
+    pfyRevealAccordionInx++;
+    const details = this.wrapIntoDetailsElement(targetEl, uid);
+    this.initRequiredFormFields(details);
+    console.debug('TargetElem prepared: ', targetEl);
+    targetEl.style.display = '';
+    targetEl.classList.forEach(cls => {
+      if (cls.includes('visually-hidden')) {
+        targetEl.classList.remove(cls);
+      }
+    })
+  } // prepareTargetElem
+
+
+  wrapIntoDetailsElement(targetEl, uid) {
+    const details = document.createElement('details');
+    const summary = document.createElement('summary');
+    const summarySpan = document.createElement('span');
+    const bodyWrapper = document.createElement('div');
+
+    details.id = uid;
+    details.className = `mdp-accordion mdp-accordion-wrapper ${uid}`;
+
+    if (this.options.class) {
+      details.classList.add(this.options.class);
+    }
+
+    if (this.options.frame) {
+      details.classList.add('mdp-border');
+      if (typeof this.options.frame === 'string') {
+        details.setAttribute('style', `--mdp-accordion-details-border-color: ${this.options.frame};`);
+      }
+    }
+
+    if (this.options.shadow) {
+      details.classList.add('mdp-accordion-shadow');
+    }
+
+    this.applyIconAttributes(summary);
+    this.applyIconRotationAttributes(summary);
+
+    bodyWrapper.className = 'mdp-accordion-body';
+    summary.setAttribute('tabindex', '-1');
+    summarySpan.textContent = this.options.label;
+    summary.appendChild(summarySpan);
+
+    targetEl.parentNode.insertBefore(details, targetEl);
+    details.appendChild(summary);
+    bodyWrapper.appendChild(targetEl);
+    details.appendChild(bodyWrapper);
+
+    return details;
+  } // wrapIntoDetailsElement
+
+
+  // === setup triggers =========================================================
     setupGlobalListeners() {
         if (pfyRevealAccordionListenersInitialized) {
             return;
         }
         pfyRevealAccordionListenersInitialized = true;
 
-        ['change', 'input'].forEach((eventType) => {
-            document.body.addEventListener(eventType, (ev) => {
-              const el = ev.target;
-              if (eventType === 'input' && (el.type === 'radio' || el.type === 'checkbox')) {
-                return;
-              } else if (eventType === 'change' && !(el.type === 'radio' || el.type === 'checkbox')) {
-                return;
-              }
-              let controllerEl = el.closest('.mdp-accordion-controller');
-              if (controllerEl) {
-                this.handleAccordionTrigger(controllerEl);
-              } else {
-                // if it's not a controller, it could still be a radio button associated with a controller:
-                controllerEl = this.resolveTriggeredController(el);
-                if (!controllerEl) {
-                  return;
-                }
-                this.handleAccordionTrigger(controllerEl, false);
-              }
-            });
-        });
+        this.setupStatefullListeners();
+        this.setupStatelessListeners();
     } // setupGlobalListeners
 
 
-    resolveTriggeredController(elem) {
-      let matchedController = null;
-      if (elem.type === 'radio' && elem.name) {
-            domForOne(`[name=${elem.name}]`, (el) => {
-                if (!matchedController && el.classList.contains('mdp-accordion-controller')) {
-                  matchedController = el;
-                }
-            });
-        }
-        return  matchedController;
-    } // resolveTriggeredController
+    setupStatefullListeners() {
+      ['change', 'input'].forEach((eventType) => {
+        document.body.addEventListener(eventType, (ev) => {
+          const el = ev.target;
+          let controllerEl = el.closest('.mdp-accordion-controller');
+          if (controllerEl && this.isStatelessTriggerElement(controllerEl)) {
+            return; // ignore stateless triggers, they are handled separately
+          }
 
-
-    setupGlobalClickListeners() {
-        if (pfyRevealAccordionClickListenersInitialized) {
-            return;
-        }
-        pfyRevealAccordionClickListenersInitialized = true;
-
-        document.addEventListener('click', (ev) => {
-            const controllerEl = ev.target.closest('.mdp-accordion-controller');
-            if (!controllerEl || !this.isClickableElement(controllerEl)) {
-                return;
+          if (controllerEl) {
+            this.handleStatefullTrigger(controllerEl);
+          } else {
+            // if it's not a controller, it could still be a radio button associated with a controller:
+            controllerEl = this.findRelatedRadioController(el);
+            if (!controllerEl) {
+              return;
             }
-
-            if (controllerEl.tagName === 'A') {
-                ev.preventDefault();
-            }
-
-            const isPressed = controllerEl.getAttribute('aria-pressed') !== 'false';
-            controllerEl.setAttribute('aria-pressed', String(!isPressed));
-            this.handleAccordionTrigger(controllerEl);
+            this.handleStatefullTrigger(controllerEl, false);
+          }
         });
-    } // setupGlobalClickListeners
+      });
+    } // setupStatefullListeners
 
 
-    handleAccordionTrigger(controllerEl, newState) {
-        if (typeof newState === 'undefined') {
-          newState = this.evalAllControllerStates();
+    setupStatelessListeners() {
+      document.addEventListener('click', (ev) => {
+        const controllerEl = ev.target.closest('.mdp-accordion-controller');
+        if (!controllerEl || !this.isStatelessTriggerElement(controllerEl)) {
+          return; // ignore stateless triggers, they are handled separately
         }
-        const targetIds = controllerEl.getAttribute('aria-controls').split(' ');
 
-        targetIds.forEach((targetId) => {
-            const targetEl = document.getElementById(targetId);
-            this.setNewState(targetEl, newState);
-        });
-    } // handleAccordionTrigger
+        this.handleStatelessTrigger(ev, controllerEl);
+      });
+    } // setupStatelessListeners
 
 
-    evalAllControllerStates() {
-      if (!this.options.controller) {
-        return false;
+
+  // === trigger handlers ========================================================
+    handleStatefullTrigger(controllerEl, forceState) {
+      if (controllerEl.tagName === 'INPUT' && (controllerEl.type === 'radio' || controllerEl.type === 'checkbox')) {
+        controllerEl = this.findRelatedRadioController(controllerEl);
       }
 
+      if (typeof forceState === 'undefined') {
+        forceState = this.evalAllControllerStates(controllerEl);
+      }
+
+      this.handleAccordionTrigger(controllerEl, forceState);
+    } // handleStatefullTrigger
+
+
+    handleStatelessTrigger(ev, controllerEl) {
+      if (controllerEl.tagName === 'A') {
+        ev.preventDefault();
+      }
+      const isPressed = controllerEl.getAttribute('aria-pressed') === 'true';
+      controllerEl.setAttribute('aria-pressed', String(!isPressed));
+      const newState = this.evalAllControllerStates(controllerEl);
+      this.handleAccordionTrigger(controllerEl, newState);
+    } // handleStatelessTrigger
+
+
+
+  handleAccordionTrigger(controllerEl, newState) {
+    const attrs = controllerEl.getAttribute('aria-controls');
+    let targetIds;
+    if (attrs && attrs.length) {
+      targetIds = attrs.split(' ');
+    } else {
+      const dataTarget = controllerEl.dataset.revealTarget;
+      if (!dataTarget) {
+        return;
+      }
+      targetIds = dataTarget.split(' ');
+    }
+
+    targetIds.forEach((targetSel) => {
+      const ch1 = targetSel.charAt(0);
+      if (ch1 !== '.' && ch1 !== '#') {
+        targetSel = '#' + targetSel;
+      }
+      domForAll(targetSel, targetEl => {
+        this.setNewState(targetEl, newState);
+      });
+    });
+  } // handleAccordionTrigger
+
+
+  evalAllControllerStates(controllerEl) {
+      const targets = controllerEl.dataset.revealTarget;
       let newState = false;
-      domForAll(this.options.controller, (el) => {
-          newState = newState || this.evalControllerState(el);
+      domForAll('[data-reveal-target]', (el) => {
+        el = this.findRelatedRadioController(el);
+        const targ = el.dataset.revealTarget;
+        if (targ !== targets) {
+          return;
+        }
+        newState = newState || this.evalControllerState(el);
       });
 
       return newState;
@@ -186,8 +272,9 @@ class RevealAccordion {
             return el.value.trim() !== '';
         }
 
-        if (RevealAccordion.interactiveControllerTags.includes(tag)) {
-            return el.getAttribute('aria-pressed') !== 'false';
+        if (this.isStatelessTriggerElement(el)) {
+          const pressed = el.getAttribute('aria-pressed');
+          return !!pressed && pressed !== 'false';
         }
 
         return false;
@@ -213,7 +300,20 @@ class RevealAccordion {
     } // evalInputControllerState
 
 
-    setNewState(targetEl, newState) {
+  findRelatedRadioController(elem) {
+    let matchedController = elem;
+
+    // for radio group: check all siblings, find the one that carries .mdp-accordion-controller:
+    if (elem.type === 'radio' && elem.name) {
+      domForAll(`[name=${elem.name}]`, (el) => {
+        matchedController = el;
+      });
+    }
+    return  matchedController;
+  } // findRelatedRadioController
+
+
+  setNewState(targetEl, newState) {
         const detailsEl = targetEl.closest('details');
         if (!detailsEl || detailsEl.open === newState) {
             return;
@@ -238,47 +338,6 @@ class RevealAccordion {
             el.required = setRequired;
         });
     } // handleRequiredFormFields
-
-
-    wrapIntoDetailsElement(targetEl, uid) {
-        const details = document.createElement('details');
-        const summary = document.createElement('summary');
-        const summarySpan = document.createElement('span');
-        const bodyWrapper = document.createElement('div');
-
-        details.id = uid;
-        details.className = `mdp-accordion ${uid}`;
-
-        if (this.options.class) {
-            details.classList.add(this.options.class);
-        }
-
-        if (this.options.frame) {
-            details.classList.add('mdp-border');
-            if (typeof this.options.frame === 'string') {
-                details.setAttribute('style', `--mdp-accordion-details-border-color: ${this.options.frame};`);
-            }
-        }
-
-        if (this.options.shadow) {
-            details.classList.add('mdp-accordion-shadow');
-        }
-
-        this.applyIconAttributes(summary);
-        this.applyIconRotationAttributes(summary);
-
-        bodyWrapper.className = 'mdp-accordion-body';
-        summary.setAttribute('tabindex', '-1');
-        summarySpan.textContent = this.options.label;
-        summary.appendChild(summarySpan);
-
-        targetEl.parentNode.insertBefore(details, targetEl);
-        details.appendChild(summary);
-        bodyWrapper.appendChild(targetEl);
-        details.appendChild(bodyWrapper);
-
-        return details;
-    } // wrapIntoDetailsElement
 
 
     applyIconAttributes(summary) {
@@ -317,4 +376,49 @@ class RevealAccordion {
             `--mdp-icon-rotation: 0deg;--mdp-icon-rotation-open: ${this.options.iconRotation};`
         );
     } // applyIconRotationAttributes
+
+
+  // stateless trigger elements: a, button, input type=button:
+  isStatelessTriggerElement(el) {
+    const name = el.tagName;
+    return (name === 'A' || name === 'BUTTON' || (name === 'INPUT' && el.type === 'button'));
+  } // isStatelessTriggerElement
+
+
+  // === public helpers ==================================================
+  reveal(targetEl) {
+    this.setNewState(targetEl, true);
+  } // reveal
+
+  unreveal(targetEl) {
+    this.setNewState(targetEl, false);
+  } // unreveal
+
 } // RevealAccordion
+
+
+
+let pfyAccordion = null;
+
+function pfyReveal(options) {
+  pfyAccordion = new RevealAccordion(options);
+}
+
+
+function revealAccordion(targetEl) {
+  if (pfyAccordion === null) {
+    const options = RevealAccordion.defaultOptions;
+    pfyAccordion = new RevealAccordion(options);
+  }
+  pfyAccordion.reveal(targetEl);
+} // revealAccordion
+
+
+function unrevealAccordion(targetEl) {
+  if (pfyAccordion === null) {
+    const options = RevealAccordion.defaultOptions;
+    pfyAccordion = new RevealAccordion(options);
+  }
+  pfyAccordion.unreveal(targetEl);
+} // unrevealAccordion
+
